a8fbdb0e7e38e29726c2303183aa618710bc0b77
Maxime Salomon <maxime@croissant.io>
2024-08-21 17:01:38 +0200

    update nextconfig to add image path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/croissant-website',
+    assetPrefix: '/croissant-website/',
+};
 
 export default nextConfig;
